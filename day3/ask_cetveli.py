74b99898d28ef6089cd438f0f00b496569961c04
Cuneyt Kilic <cuneytkilic1@gmail.com>
2022-05-19 21:59:45 +0300

    bazı eklemeler silindi

--- day3/ask_cetveli.py
+++ day3/ask_cetveli.py
@@ -1,10 +1,7 @@
-# 🚨 Don't change the code below 👇
+
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 low_name1 = name1.lower()
 low_name2 = name2.lower()
